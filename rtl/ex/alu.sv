bd9f54259480e113aa1a008b2f351b5aad99ff20
ahpinder <42349096+ahpinder@users.noreply.github.com>
2025-06-12 16:02:41 -0400

    fixed syntax errors + assignment blocking

--- rtl/ex/alu.sv
+++ rtl/ex/alu.sv
@@ -1,11 +1,11 @@
-`include "../common/cpu_defs.svh"
+`include "common/cpu_defs.svh"
 
 module alu (
-    input logic clk;
-    input logic rst;
-    input instruction_t inst_in;
-    output instruction_t inst_out;
-    output logic [31:0] result_out;
+    input logic clk,
+    input logic rst,
+    input instruction_t inst_in,
+    output instruction_t inst_out,
+    output logic [31:0] result_out
 );
     logic [31:0] a;
     logic [31:0] b;
@@ -41,7 +41,7 @@ module alu (
         end
         else begin
             result <= 32'b0;
-            inst_out_ff = '0;
+            inst_out_ff <= '0;
         end
     end
     assign inst_out = inst_out_ff;
